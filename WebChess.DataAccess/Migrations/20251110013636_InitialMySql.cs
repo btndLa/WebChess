022d541682d82b149eab75d2504800df791d5e61
Laczkovics Botond <botond.laczkovics@gmail.com>
2025-11-10 02:37:52 +0100

    Configure migrations

--- WebChess.DataAccess/Migrations/20251110013636_InitialMySql.cs
+++ WebChess.DataAccess/Migrations/20251110013636_InitialMySql.cs
@@ -5,25 +5,18 @@
 namespace WebChess.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class Playercolor : Migration
+    public partial class InitialMySql : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "TakenPieces",
-                table: "Games",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "[]");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "TakenPieces",
-                table: "Games");
+
         }
     }
 }
